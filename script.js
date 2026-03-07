2c7c9c184ba51aece741c92f540ac11771271be8
Paruky <haruki.mzawa0408@gmail.com>
2026-03-07 18:11:37 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ let prob = 0.96;
 let basketX = 0;
 let timerId = NaN;
 let basket, chick, egg1, egg2, div1, div2;
-let height = window.innerHeight;
+let height;
+let width;
+let canvas;
+let canvasSize = 600; // 自由に変更してね！canvasの大きさだよ！
+
 let scoreDisplay;
 let eggs = [];
 let back;
@@ -13,16 +17,30 @@ let back;
 onload = function() {
     ctx = document.getElementById("field").getContext("2d");
     ctx.font = "40px 'sniglet', 'Roboto', 'Arial', sans-serif";
+    
     basket = document.getElementById("basket");
     chick = document.getElementById("chick");
     egg1 = document.getElementById("egg1");
     egg2 = document.getElementById("egg2");
     back = document.getElementById("back");
+    canvas = document.getElementById("field");
+    width = window.innerWidth;
+    height = window.innerHeight;
     scoreDisplay = document.getElementById("score");
     div1 = document.getElementsByClassName("header")[0];
     div2 = document.getElementsByClassName("footer")[0];
-    div1.style.height = (height - 600) / 2 + "px";
-    div2.style.height = (height - 600) / 2 + "px";
+
+    if (width < 600) {
+        canvasSize = width;
+    }
+
+    canvas.width = canvasSize;
+    canvas.height = canvasSize;
+    
+    div1.style.height = (height - canvasSize) / 2 + "px";
+    div2.style.height = (height - canvasSize) / 2 + "px";
+    div1.style.width = canvasSize + "px";
+    div2.style.width = canvasSize + "px";
 
     timerId = setInterval(tick, 50);
     window.onmousemove = (e) => {
@@ -32,14 +50,14 @@ onload = function() {
 
 function tick() {
     ctx.drawImage(back, 0, 0);
-    ctx.drawImage(basket, basketX - 50, 500);
+    ctx.drawImage(basket, basketX - 50, canvasSize - 100);
     if (Math.random() > prob) {
-        eggs.push({x: Math.random() * 500, y: 1});
+        eggs.push({x: Math.random() * (canvasSize - 100), y: 1});
     }
     let prev = eggs.length;
     eggs = eggs.filter((e) => {
         return (
-            e.y < 400 || e.y > 600 || e.x < basketX - 50 || e.x > basketX + 50
+            e.y < (canvasSize - 200) || e.y > canvasSize || e.x < basketX - 50 || e.x > basketX + 50
         );
     });
     if (prev != eggs.length) {
@@ -57,11 +75,11 @@ function tick() {
             ctx.drawImage(egg1, e.x, e.y);
         }
 
-        if (e.y > 550) {
+        if (e.y > (canvasSize - 50)) {
             clearInterval(timerId);
-            ctx.font = "90px 'sniglet', 'Roboto', 'Arial', sans-serif";
-            ctx.fillText("Game Over", 80, 300);
-            ctx.drawImage(egg2, e.x, 500);
+            ctx.font = "calc(" + canvasSize * (3 / 20) + "px) 'sniglet', 'Roboto', 'Arial', sans-serif";
+            ctx.fillText("Game Over", (canvasSize * (2 / 15)), canvasSize / 2);
+            ctx.drawImage(egg2, e.x, canvasSize - 100);
         }
     });
 }
